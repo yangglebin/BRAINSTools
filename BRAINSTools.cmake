e53e9367da131225031586955a919f29d399edfb
Hans Johnson <hans-johnson@uiowa.edu>
2015-09-01 17:07:52 -0500

    WIP: Test shared library RPATH settings
    Shared libraries are tricky.  We need to enable proper
    RPATH settings.

--- BRAINSTools.cmake
+++ BRAINSTools.cmake
@@ -155,6 +155,31 @@ set(TestData_DIR ${CMAKE_CURRENT_SOURCE_DIR}/TestData)
 # choose between using HDF5 or MAT format transform files
 set(XFRM_EXT "h5" CACHE STRING "Choose the preferred transform file format")
 
+#####
+## Setup RPATH for shared libraries
+## http://www.cmake.org/Wiki/CMake_RPATH_handling#Mac_OS_X_and_the_RPATH
+##
+# use, i.e. don't skip the full RPATH for the build tree
+set(CMAKE_SKIP_BUILD_RPATH  FALSE)
+
+# when building, don't use the install RPATH already
+# (but later on when installing)
+set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
+
+set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+
+# add the automatically determined parts of the RPATH
+# which point to directories outside the build tree to the install RPATH
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+
+
+# the RPATH to be used when installing, but only if it's not a system directory
+list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
+if("${isSystemDir}" STREQUAL "-1")
+   set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+endif("${isSystemDir}" STREQUAL "-1")
+
+
 #-----------------------------------------------------------------------------
 # BRAINSCommonLib (Required)
 #-----------------------------------------------------------------------------
